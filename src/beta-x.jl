2a79e95d2fe2f9bfefcede5f33e91a2ebb8dd798
Gabriel Ferreira <gabrielferreira@MacBook-Air-de-Gabriel.local>
2024-09-24 17:51:00 -0300

    Beta-X update

--- src/beta-x.jl
+++ src/beta-x.jl
@@ -26,6 +26,8 @@
 #
 #           -- As descrições disso estão ali pela linha 150 +/-
 #
+#       OBS: Conforme eu fui escrevendo o código os comentários ficaram mais avulsos, estou a disposição para quem tiver dúvidas =3
+#
 # ================================================================================================================================= #
 #       - Bibliotecas necessárias para rodar esse script:
 #           > Obviamente a RP.jl é uma dependência, sendo ela o arquivo `rp.jl` presente junto com esse =3
@@ -35,37 +37,39 @@ include("rp.jl")
 # ================================================================================================================================= #
 using .RP
 using JLD2
-using BSON
 using Flux
+using Metal         # Trocas esse pelo CUDA ou AMDGPU dependendo da placa de vídeo =3
 using Dates
 using Statistics
+using CairoMakie
 using ProgressMeter
+using LinearAlgebra
 using BenchmarkTools
 # ================================================================================================================================= #
 #       - Aqui tem algumas constantes para ajudar na configuração facilitada =3
 
 #           Valores de Beta para a classificação.
-const β_values = [1.99, 2.39, 2.69, 2.99, 3.39, 3.69, 3.99, 4.39, 4.69, 4.99, 5.39, 5.69, 5.99, 6.39, 6.69, 6.99, 7.39, 7.49, 7.99, 8.39]
+const β_values = [2.59, 2.99, 3.59, 3.99, 4.59, 4.99, 5.59, 5.99, 6.59, 6.99]
 
 #           Tamanho das séries temporais que serão usadas.
-const timeseries_size = 1000
+const timeseries_size = 500
 
 #           Épocas de treinamento para a rede neural.
 const epochs = 50
 
 #           Tamanho dos motifs (microestados)
-const motif_size = 3
+const motif_size = 2
 
 #           Número de amostras para as redes neurais.
-const mlp_samples = 20
+const mlp_samples = 1
 
 #           Intervalo de threshold que vai ser usado (fica mais fácil deixar isso pronto =3)
 #   O terceiro valor disso determina a resolução, mas como o objetivo aqui é calcular a accuracy para todas as entropias
 #   deixar uma resolução muito alta vai fazer o computador chorar com a carga de trabalho >.<
-const ε = range(0, 0.9995, 100)
+const ε = range(0, 0.9995, 10)
 
 #           Tempo de execução mínimo em segundos, aproximadamente....
-const run_time = 2
+const run_time = 520.36
 
 # ================================================================================================================================= #
 #           Função de entrada para o programa. É melhor colocar tudo dentro de funções, já que o escopo global do julia
@@ -74,7 +78,7 @@ function main()
     #           Valores para calcular a entropia média =3
     xo_to_entropy = range(0.00001, 0.99999, 5)
     #           Valores para o treinamento da rede.
-    xo_to_train_mlp = rand(Float64, 3000)
+    xo_to_train_mlp = rand(Float64, 1500)
     #           Valores para o teste da rede.
     xo_to_test_mlp = rand(Float64, floor(Int, length(xo_to_train_mlp) / 3))
 
@@ -160,34 +164,184 @@ function main()
     #           Vamos começar a explorar o computador =D
     #       (e a simetria também ...)
     @showprogress for max_index = status[1]:length(ε)
-        for min_index = 1:(max_index-1)
-            #       Temos 2 tarefas aqui, a primeira é calcular a entropia e a segunda a accuracy quando aplicado a uma MLP.
-            #       Bom.. para tentar agilizar um pouco o processo, vou separar isso em 2 corrotinas, uma para a entropia
-            #   e outra para a rede. Eu podia separar ainda mais o processo, ao nível dos threshold, mas acho que fazer a
-            #   sincronização disso vai ser um inferno, então, vou-me recusar a fazer, o PC que se resolva =V
+        for min_index = status[2]:(max_index-1)
+            #
+            #       - Primeiro calcula a entropia, que demora menos =3
+            etrpy = calculate_entropy(ε[min_index], ε[max_index]; pvec=pow_vector)
+            etr_obj = load_object("data/beta-x-entropy.dat")
+            etr_obj[min_index, max_index, :] .= etrpy
+            save_object("data/beta-x-entropy.dat", etr_obj)
 
-            #       Como vou trabalhar com processos assíncronos ...
-            @elapsed begin
-                task_entropy = @task calculate_entropy(ε[min_index], ε[max_index]; pvec=pow_vector)
+            etrpy = Nothing
+            etr_obj = Nothing
 
-                schedule(task_entropy)
+            #
+            #       - Vamos para a accuracy agora...
+            accr = calculate_accuracy(ε[min_index], ε[max_index]; pvec=pow_vector)
+            acr_obj = load_object("data/beta-x-accuracy.dat")
+            acr_obj[min_index, max_index, :, :] .= accr
+            save_object("data/beta-x-accuracy.dat", acr_obj)
 
-                s = fetch(task_entropy)
+            accr = Nothing
+            acr_obj = Nothing
 
-                etr_obj = load_object("data/beta-x-entropy.dat")
-                etr_obj[min_index, max_index, :] .= s
-                save_object("data/beta-x-entropy.dat", etr_obj)
-
-                etr_obj = Nothing
-            end
+            status[2] += 1
+            save_object("data/beta-x-status.dat", status)
         end
         status[1] += 1
+        status[2] = 1
         save_object("data/beta-x-status.dat", status)
     end
-
-    println(status)
+    # ============================================================================================================================= #
+    make_graphs()
 end
 # ================================================================================================================================= #
+function make_graphs()
+    #       Gráfico da accuracy
+    accr_mat = load_object("data/beta-x-accuracy.dat")
+    heatmap(accr_mat[:, :, end, 1], colormap=Reverse(:deep))
+end
+# ================================================================================================================================= #
+#           Calcula a accuracy =3
+function calculate_accuracy(ε_min, ε_max; pvec=power_vector(motif_size))
+
+    #       Carrega os dados.
+    test_serie = load_object("data/beta-x-mlp-test-serie.dat")
+    train_serie = load_object("data/beta-x-mlp-train-serie.dat")
+    test_sz = size(test_serie, 3)
+    train_sz = size(train_serie, 3)
+
+    #       Aloca espaço para salvar as probabilidades.
+    test_probs = zeros(Float64, 2^(motif_size * motif_size), test_sz * length(β_values))
+    train_probs = zeros(Float64, 2^(motif_size * motif_size), train_sz * length(β_values))
+
+    #       Agora para as labels...
+    test_labels = ones(Float64, test_sz * length(β_values))
+    train_labels = ones(Float64, train_sz * length(β_values))
+
+    #
+    #       Vou criar uma função semelhante a __async_calculate_entropy que usei para o calculate_entropy,
+    #   só que essa aqui retorna as probabilidades... o que fica um pouquinho mais chato já que tem
+    #   que redimensionar o Dict para uma Array ... apesar de não ser tão complicado fazer isso...
+    function __async_calculate_probs(data, ε_tuple; pvec=power_vector(motif_size))
+        sz = size(data, 3)
+        async_result = zeros(Float64, 2^(motif_size * motif_size), sz)
+        for i = 1:sz
+            rp = recurrence_matrix(data[:, :, i], ε_tuple; recurrence=RP.corridor_recurrence)
+            probs, _ = motifs_probabilities(rp, motif_size; no_use_samples=false, power_vector=pvec)
+            async_result[collect(keys(probs)), i] .= collect(values(probs))
+        end
+
+        return async_result
+    end
+
+    #
+    #       Vou criar uma função para efetivamente calcular a accuracy a partir da rede =3
+    function __calc_accurarcy(predicted, trusty)
+        #       Gera uma matriz de confusão...
+        conf = zeros(Int, length(β_values), length(β_values))
+        sz = size(predicted, 2)
+
+        for i = 1:sz
+            mx_prd = findmax(predicted[:, i])
+            mx_trt = findmax(trusty[:, i])
+            conf[mx_prd[2], mx_trt[2]] += 1
+        end
+
+        return tr(conf) / sum(conf)
+    end
+
+    #
+    #       Treinamento assíncrono =v
+    function __async_net_train(mlp_index, data_loader, test_probs, test_labels)
+        accr = zeros(Float64, epochs)
+        model = load_object(string("network/beta-x-", mlp_index, ".mlp")) |> gpu
+        model_state = Flux.setup(Flux.Adam(0.0001), model)
+
+        for epc = 1:epochs
+            for (x, y) in data_loader
+                _, grads = Flux.withgradient(model) do m
+                    y_hat = m(x)
+                    Flux.logitcrossentropy(y_hat, y)
+                end
+                Flux.update!(model_state, model, grads[1])
+            end
+
+            #       Calcula o accuracy...
+            accr[epc] = __calc_accurarcy(model(test_probs) |> cpu, test_labels)
+        end
+
+        return accr
+    end
+
+    for beta in eachindex(β_values)
+        #
+        #       Separa as tarefas em blocos para as probabilidades de treinamento...
+        train_chuncks = Iterators.partition(1:train_sz, train_sz ÷ Threads.nthreads())
+        train_tasks = map(train_chuncks) do chunck
+            Threads.@spawn __async_calculate_probs(train_serie[:, :, chunck, beta], (ε_min, ε_max); pvec=power_vector(motif_size))
+        end
+
+        train_segment = fetch.(train_tasks)
+
+        test_chunck = Iterators.partition(1:test_sz, test_sz ÷ Threads.nthreads())
+        test_tasks = map(test_chunck) do chunck
+            Threads.@spawn __async_calculate_probs(test_serie[:, :, chunck, beta], (ε_min, ε_max); pvec=power_vector(motif_size))
+        end
+
+        test_segment = fetch.(test_tasks)
+
+        __temp_train = zeros(Float64, 2^(motif_size * motif_size), train_sz)
+        __temp_test = zeros(Float64, 2^(motif_size * motif_size), test_sz)
+
+        for i = 1:Threads.nthreads()-1
+            __temp_train[:, 1+(i-1)*size(train_segment[i], 2):i*size(train_segment[i], 2)] .= train_segment[i]
+            __temp_test[:, 1+(i-1)*size(test_segment[i], 2):i*size(test_segment[i], 2)] .= test_segment[i]
+        end
+
+        __temp_train[:, end-(size(train_segment[Threads.nthreads()], 2)-1):end] .= train_segment[Threads.nthreads()]
+        __temp_test[:, end-(size(test_segment[Threads.nthreads()], 2)-1):end] .= test_segment[Threads.nthreads()]
+
+        train_probs[:, 1+(beta-1)*train_sz:beta*train_sz] .= __temp_train
+        test_probs[:, 1+(beta-1)*test_sz:beta*test_sz] .= __temp_test
+
+        train_labels[1+(beta-1)*train_sz:beta*train_sz] *= β_values[beta]
+        test_labels[1+(beta-1)*test_sz:beta*test_sz] *= β_values[beta]
+    end
+
+    #       Partiu liberar a memória...
+    test_serie = Nothing
+    train_serie = Nothing
+
+    #       Prepara para as redes neurais =3
+    test_labels = Flux.onehotbatch(test_labels, β_values)
+    train_labels = Flux.onehotbatch(train_labels, β_values)
+
+    data_loader = Flux.DataLoader((train_probs, train_labels), batchsize=50, shuffle=true) |> gpu
+    test_probs = test_probs |> gpu
+
+    #       Aloca espaço para registrar a accuracy...
+    accuracy = zeros(Float64, epochs, mlp_samples)
+
+    #       Carrega as redes neurais e treina...
+    #train_tasks = map(1:mlp_samples) do chunck
+    #    Threads.@spawn __async_net_train(chunck, data_loader, test_probs, test_labels)
+    #end
+
+    #mlp_accr = fetch.(train_tasks)
+
+    #       !!! Eu vou supor que o número de redes a serem treinadas é MENOR OU IGUAL ao número de Threads disponíveis !!!
+    #       Se isso não for válido, tem que arrumar o código =V
+    #       Fiz isso pq é mais fácil =P
+    for mlp in 1:mlp_samples
+        accuracy[:, mlp] .= __async_net_train(mlp, data_loader, test_probs, test_labels)
+    end
+
+    #       Retorna a accuracy
+    return accuracy
+end
+# ================================================================================================================================= #
+#           Calcula a entropia =3
 function calculate_entropy(ε_min, ε_max; pvec=power_vector(motif_size))
 
     #   Carrega os dados para o cálculo da entropia.
